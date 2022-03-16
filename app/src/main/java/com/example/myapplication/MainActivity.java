b54c0f0d2d08060e55c0e0085f484cab54e467d5
vdaya <vinay18117@iiitd.ac.in>
2022-03-16 13:40:51 +0530

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
+    Button sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("login_page", "Open");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        sign_up = findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // check if username and password available and correct.
+                Intent intent = new Intent(getApplicationContext(), MainScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterName.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterName.java
+++ app/src/main/java/com/example/myapplication/RegisterName.java
@@ -3,12 +3,48 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegisterName extends AppCompatActivity {
+    Button sub;
 
+    private EditText name;
+    private EditText id;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_name);
+        sub = findViewById(R.id.submit);
+        name = findViewById(R.id.name);
+        id = findViewById(R.id.user_id);
+        password = findViewById(R.id.user_password);
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore firebaseFirestore;
+                DocumentReference ref;
+                firebaseFirestore = FirebaseFirestore.getInstance();
+
+                ref = firebaseFirestore.collection("user").document();
+                Map<String, Object> mapper = new HashMap<>();
+
+                mapper.put("Name", name.getText().toString());
+                mapper.put("id", id.getText().toString());
+                mapper.put("password", password.getText().toString());
+                firebaseFirestore.collection("user").add(mapper);
+            }
+        });
     }
 }
